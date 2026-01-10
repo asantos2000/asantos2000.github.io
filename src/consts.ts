39a3bd4850176f4fb4ef3acf1cafd3e73eb42a83
Anderson Santos <adsantos@gmail.com>
2026-01-10 19:59:56 -0300

    fix blog name

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'adsantos';
-export const SITE_DESCRIPTION = 'A personal developer blog about Python, TypeScript, Godot, and Flutter.';
+export const SITE_TITLE = 'asantos2000';
+export const SITE_DESCRIPTION = 'Bare bones personal developer blog about software development, artificial intelligence, and methods.';
